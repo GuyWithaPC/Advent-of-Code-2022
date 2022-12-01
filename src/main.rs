96ff6a60c6a688c5b0e8cb6c58f70282a03f892c
Max Petersen <maxvonpetersen@gmail.com>
2022-11-30 21:18:49 -0700

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+mod utils;
+
+const SESSION_COOKIE: &str = "53616c7465645f5f550a96ca49a78e304d8621323b0ec4041cf3a7d5026e71a983c550d64b958c3c9ecf146201f0ff3b; session=53616c7465645f5fa7963d224672a5fa078e5f9acd3af16945247bdb62702e8adf7051b8d8d3ee83ea9d7054c97d0f034398171c41ec84ae739181134d654314";
+
+fn main() {
+    let data = utils::get_challenge_data(6);
+    println!(data);
+}
---
+++ src/utils.rs
@@ -0,0 +1,32 @@
+
+use crate::SESSION_COOKIE;
+use ureq;
+use std::fs::{self,File};
+use std::io::{Write,Read};
+use std::path::Path;
+
+pub fn get_challenge_data(day: usize) -> String {
+    let file_string = format!("inputcache/day{}.txt",day);
+    let file_path = Path::new(&file_string);
+    let mut data = String::new();
+    if file_path.exists() {
+        println!("reading data from cache...");
+        let mut buf = Vec::new();
+        File::open(file_path)
+            .expect("Failed to open cached data file!")
+            .read_to_end(&mut buf).expect("Failed to read cached data file!");
+        data = String::from_utf8(buf).expect("Failed to parse cached data file!");
+
+    } else {
+        println!("cache not found! performing GET request...");
+        data = ureq::get(&format!("https://adventofcode.com/2022/day/{}/input", day))
+            .set("cookie", &format!("ru={}", SESSION_COOKIE))
+            .call().expect("HTTP GET request failed")
+            .into_string().expect("Failed to grab a string");
+        File::create(file_path)
+            .expect("Unable to create cache file!")
+            .write_all(data.as_bytes())
+            .expect("Unable to write to cache file!");
+    }
+    return data
+}
